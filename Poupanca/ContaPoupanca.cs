2113d4743fa2fca746ddb042975bb1ccd1cab4fa
Diego Augusto Barros <39376538+profdiegoaugusto@users.noreply.github.com>
2018-09-29 12:40:59 -0300

    Commit inicial

---
+++ Poupanca/ContaPoupanca.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Poupanca
+{
+    class ContaPoupanca
+    {
+        private decimal saldoPoupanca;
+        private static double taxaDeJurosAnual;
+
+        private const int MESES = 12;
+
+        /// <summary>
+        /// Cria uma nova conta poupança
+        /// </summary>
+        public ContaPoupanca()
+        {
+            saldoPoupanca = 0.0M;
+            taxaDeJurosAnual = 0.0;
+        }
+
+        /// <summary>
+        /// Cria uma nova conta poupança
+        /// </summary>
+        /// <param name="saldoPoupanca">O valor do saldo da poupança</param>
+        public ContaPoupanca(decimal saldoPoupanca)
+        {
+            this.saldoPoupanca = saldoPoupanca;
+            taxaDeJurosAnual = 0.0;
+        }
+
+        /// <summary>
+        /// Pega ou configura o saldo atual da poupança
+        /// </summary>
+        public decimal Saldo
+        {
+            get { return saldoPoupanca; }
+            set { saldoPoupanca = value; }
+        }
+
+        /// <summary>
+        /// Pega ou configura a taxa de juros atual da poupança
+        /// </summary>
+        public static double TaxaJuros
+        {
+            get { return taxaDeJurosAnual; }
+            set { taxaDeJurosAnual = value; }
+        }
+
+        /// <summary>
+        /// Calcula o juros mensais da poupança acrescidos do saldo atual
+        /// </summary>
+        /// <returns>O valor decimal do novo saldo dos poupadores</returns>
+        public decimal CalcularJurosMensal()
+        {
+            decimal jurosMensal = (saldoPoupanca * (decimal)taxaDeJurosAnual) / MESES;
+            return jurosMensal + saldoPoupanca;
+        }
+
+        /// <summary>
+        /// Altera a taxa de juros da conta poupança
+        /// </summary>
+        /// <param name="taxaDeJuros">Novo valor double para taxa de Juros</param>
+        public static void AlterarTaxaDeJuros(double taxaDeJuros)
+        {
+            taxaDeJurosAnual = taxaDeJuros;
+        }
+
+    } // Fim da classe ContaPoupanca
+
+} // Fim do namespace Poupanca
---
+++ Poupanca/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Poupanca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaPoupanca poupador1 = new ContaPoupanca(2000M);
+            ContaPoupanca poupador2 = new ContaPoupanca(3000M);
+            decimal saldoPoupador1 = 0.0M;
+            decimal saldoPoupador2 = 0.0M;
+
+            // Altera taxa de juros da poupança de todos correntistas
+            Console.WriteLine("Taxa de Juros = 4%");
+            ContaPoupanca.AlterarTaxaDeJuros(0.04);
+
+            // Calcula saldo mensal para a nova taxa de juros
+            saldoPoupador1 = poupador1.CalcularJurosMensal();
+            saldoPoupador2 = poupador2.CalcularJurosMensal();
+            Console.WriteLine("   Poupador 1 = {0:C}", saldoPoupador1);
+            Console.WriteLine("   Poupador 2 = {0:C}", saldoPoupador2);
+
+
+            Console.WriteLine("\nTaxa de Juros = 5%");
+            ContaPoupanca.AlterarTaxaDeJuros(0.05);
+
+            saldoPoupador1 = poupador1.CalcularJurosMensal();
+            saldoPoupador2 = poupador2.CalcularJurosMensal();
+            Console.WriteLine("   Poupador 1 = {0:C}", saldoPoupador1);
+            Console.WriteLine("   Poupador 2 = {0:C}", saldoPoupador2);
+
+
+            Console.Write("\n\nPressione qualquer tecla para sair . . .");
+            Console.ReadKey(true);
+
+        } // Fim do método Main
+
+    } // Fim da classe Program
+
+} // Fim do namespace Poupanca
